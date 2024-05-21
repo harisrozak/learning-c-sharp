a2da77a9a0b3afce0901dcae430efb57fca880c2
Haris Ainur Rozak <harisrozak@gmail.com>
2024-05-21 16:06:14 +0700

    added ListIntroduction

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Exercise Exercise = new Exercise();
-            Exercise.MathQuestion();            
+            CodeLearning CodeLearning = new CodeLearning();
+            CodeLearning.ListIntroduction();
+
+            // Exercise Exercise = new Exercise();
+            // Exercise.MathQuestion();            
         }
     }
 
@@ -73,9 +76,12 @@ namespace MyFirstProject
             System.Console.WriteLine();
 
             System.Console.Write("Enter your age: ");
-            int newAge = Convert.ToInt32(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out int newAge);
 
             System.Console.WriteLine("Your name is " + newName + " and your age is " + newAge);
+            System.Console.WriteLine($"Your name is {newName} and your age is {newAge}");
+            System.Console.WriteLine(string.Format("Your name is {0} and your age is {1}", newName, newAge));
+            System.Console.WriteLine(string.Concat("Your name is ", newName, " and your age is ", newAge));
 
             if (newAge == 18)
             {
@@ -96,6 +102,129 @@ namespace MyFirstProject
             System.Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB"))); // -£3.33
             System.Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); // -$3.33
             System.Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("id-ID"))); // -Rp3,33
+        }
+
+        public void StringVerbatim() {
+            string NormalString = "C:\\Users\\Lorem\\Ipsum\\Dolor\nNew Line Test\tTab Test";
+            string VerbatimString = @"C:\Users\Lorem\Ipsum\Dolor Sit Amet";
+
+            System.Console.WriteLine(NormalString);
+            System.Console.WriteLine(VerbatimString);
+        }
+
+        public void StringComparation() {
+            System.Console.WriteLine("Enter your name:");
+            string name = System.Console.ReadLine();
+            string correctName = "Aba";
+
+            if (string.IsNullOrEmpty(name)) {
+                System.Console.WriteLine("Your name is empty");
+            }
+            else if(name.Equals(correctName)) {
+                System.Console.WriteLine($"Your name is correct, it's {correctName}");
+            }
+            else {
+                System.Console.WriteLine($"Your name is {name} instead of {correctName}");
+            }
+        }
+
+        public void ArrayIntroduction() {
+            int[] numbers = new int[4] // Set the length of the array, in this case is 4
+            {
+                1, 2, 3, 4 // Set the array values
+            };
+            
+            // Replace the original values
+            numbers[0] = 16;
+            numbers[3] = 128;
+            numbers[1] = 32;
+            numbers[2] = 64;
+
+            // Sort the array values
+            Array.Sort(numbers); // 16, 32, 64, 128
+
+            // Reserve the array values
+            Array.Reverse(numbers); // 128, 64, 32, 16
+
+            System.Console.WriteLine("Iterate using For:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                System.Console.WriteLine(numbers[i]);
+            }
+
+            System.Console.WriteLine("Iterate using Foreach:");
+
+            foreach (int num in numbers)
+            {
+                System.Console.WriteLine(num);
+            }
+        }
+
+        public void ArraySearch() {
+            int[] numbers = new int[]
+            {
+                1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+            };
+
+            System.Console.WriteLine("Enter a fibonacci number to search:");
+            bool isNumberToSearch = int.TryParse(Console.ReadLine(), out int numberToSearch);
+
+            if (!isNumberToSearch) 
+            {
+                System.Console.WriteLine("Invalid number to search");
+            }
+            else 
+            {
+                int position = Array.IndexOf(numbers, numberToSearch);
+
+                if (position < 0) 
+                {
+                    System.Console.WriteLine($"Number {numberToSearch} has not been found");
+                }
+                else
+                {
+                    System.Console.WriteLine($"Number {numberToSearch} has been found at position {position + 1}");
+                }
+            }
+        }
+
+        public void ListIntroduction() {
+            List<int> numbers = new List<int>()
+            {
+                1, 2, 3, 5
+            };
+
+            System.Console.WriteLine("This is the initial data we have:");
+
+            foreach (int number in numbers)
+            {
+                System.Console.Write($"{number} ");
+            }
+
+            int countNumbersToAdd = 3;
+
+            for (int i = 0; i < countNumbersToAdd; i++)
+            {
+                System.Console.WriteLine($"Add the {numbers.Count}th number:");
+                bool isNewNumber = int.TryParse(Console.ReadLine(), out int newNumber);
+
+                if (!isNewNumber) 
+                {
+                    System.Console.WriteLine("Invalid number. Skipped.");
+                }
+                else
+                {
+                    numbers.Add(newNumber);
+                }
+            }
+
+            System.Console.WriteLine("This is ours numbers now:");
+
+            foreach (int number in numbers)
+            {
+                System.Console.Write($"{number} ");
+            }
         }
     }
 
